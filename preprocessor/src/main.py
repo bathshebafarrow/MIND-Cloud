84c43d6234c52c01d6e0e5ee0f8882c2ec875a6d
Bathsheba Farrow <55559879+bathshebafarrow@users.noreply.github.com>
2025-06-27 22:24:48 -0400

    Delete preprocessor/src directory

--- preprocessor/src/main.py
+++
@@ -1,9 +0,0 @@
-from app.config import SQS_URL
-from worker import FileQueueWorker
-
-def processQueue():
-    with FileQueueWorker(SQS_URL) as worker:
-        worker.process_queue()
-
-if __name__ == "__main__":
-    processQueue()
