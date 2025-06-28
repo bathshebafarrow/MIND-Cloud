6af3d8325cc3cb1289f100c851204ecbae0c9c8c
Bathsheba Farrow <55559879+bathshebafarrow@users.noreply.github.com>
2025-06-27 21:36:38 -0400

    Create config.py

---
+++ frontend/src/config.py
@@ -0,0 +1,12 @@
+# AWS access key associated with an IAM role or user.
+AWS_ACCESS_KEY = ''
+# A secret key associated with the access key (password).
+AWS_SECRET_KEY = ''
+# The default region when creating new connections.
+AWS_REGION_NAME = 'us-east-1'
+# The secret key for the app.
+SECRET_KEY = ''
+
+EXTERNAL_BUCKET = 'openneuro.org'
+
+INTERNAL_BUCKET = 'eeg-data-raw'
